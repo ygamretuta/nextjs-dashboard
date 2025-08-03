6ccf8bbf4e9b7160bdd8bea681d68257d604f3fa
Ygam Retuta <ygam.j.retuta@gmail.com>
2025-08-03 15:30:39 +0800

    remove unused prop on edit not found

--- app/dashboard/invoices/[id]/edit/not-found.tsx
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { FaceFrownIcon} from "@heroicons/react/24/outline";
 
-export default function NotFound(props: any) {
+export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
       <FaceFrownIcon className="w-10 text-gray-400"/>
