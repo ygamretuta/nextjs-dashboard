2b54a55d6a257e4af54dcfc58a4130f597dbf2f5
Ygam Retuta <ygam.j.retuta@gmail.com>
2025-08-03 23:00:54 +0800

    remove matcher NextAuth word

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'], NextAuth
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
 }
